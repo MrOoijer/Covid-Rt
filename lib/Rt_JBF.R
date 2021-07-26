a2aea296d9a30291a898fb5002840d48ca0c4b74
MrOoijer <mrooijer@gmail.com>
2021-07-26 20:17:32 +0200

    Explain improvement JBF

--- lib/Rt_JBF.R
+++ lib/Rt_JBF.R
@@ -3,6 +3,10 @@
 Rt_JBF <- function(data_set, 
                    SMOOTH_DATA=11, 
                    PER_ANAL=4, SKIP= 5){
+  
+  ### call new one
+  return(Rt_JBF_2(data_set))
+  
   # smooth
   cs<- smoothed(data_set, SMOOTH_DATA)
   
@@ -28,4 +32,34 @@ Rt_JBF <- function(data_set,
   df4$date<- df4$date - 0
   # do not yet show recent values bacause unreliable
   df4<- head(df4, -SKIP)
+  df4
+}
+
+Rt_JBF_2 <- function(data_set)
+{
+  N= 7500
+  
+  LO<- 0.4
+  HI<- 10
+  
+  trend_all <- smoothed(data_set, 11)
+  M<- matrix(0,nrow=nrow(trend_all), ncol=N)
+  for ( m in 1:nrow(trend_all)) 
+    M[m,] <- rnorm(N, trend_all[m,2], 0.0263*trend_all[m,2])
+  
+  M1<- head(M, -4)
+  M<- ifelse (M1 >0, tail(M, -4)/M1, 0)
+  
+  R<-  apply(M, 1, function(x){
+    x<- x[x<HI & x > LO]
+    mean(x)
+  })
+  STD<- apply(M, 1, function(x){
+    x<- x[x<HI & x > LO]
+    sd(x)
+  })
+  
+  result<- data.frame(date= head(trend_all$date, -4), 
+                      R=R, lo= R-1.96*STD, hi=R+1.96*STD)
+  return(head(result, -5))
 }
